96268c2ed8f419bbe8c91985749a3aa12c25558b
adhamcahyo <nebelwerfersf@gmail.com>
2023-07-18 19:03:25 +0700

    fix uncommited

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,21 +1,17 @@
 <?php
 
 namespace App\Models;
-<<<<<<< HEAD
 
-=======
->>>>>>> 2fa551aadc82ae4c817812d74f69231d1c3e59bc
+
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-<<<<<<< HEAD
+
  class User extends Authenticatable //implements MustVerifyEmail
-=======
-class User extends Authenticatable//implements MustVerifyEmail
->>>>>>> 2fa551aadc82ae4c817812d74f69231d1c3e59bc
+
 {
     use HasApiTokens, HasFactory, Notifiable;
 
